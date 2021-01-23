a42740a1e8f0f751e8e35d78594375a4cc12ff98
Zshuangshuang <19990306Zss>
2021-01-23 21:16:14 +0800

    提交代码

---
+++ OJ习题合集/src/com/ssz/寻找第K大/Solution.java
@@ -0,0 +1,11 @@
+package com.ssz.寻找第K大;
+
+/**
+ * Author:ZouDouble
+ * Description:
+ * 天气：晴天
+ * 目标：Good Offer
+ * Date    2021-01-23 20:43
+ */
+public class Solution {
+}
---
+++ OJ习题合集/src/com/ssz/统计回文串/Main.java
@@ -0,0 +1,11 @@
+package com.ssz.统计回文串;
+
+/**
+ * Author:ZouDouble
+ * Description:
+ * 天气：晴天
+ * 目标：Good Offer
+ * Date    2021-01-23 20:12
+ */
+public class Main {
+}
